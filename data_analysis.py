5e6258ddbeb1ed76bc975204836b541c3cc73fb7
richard <richard.bille@fu-berlin.de>
2023-10-18 19:51:36 +0200

    reading Data

--- data_analysis.py
+++ data_analysis.py
@@ -4,3 +4,6 @@ import matplotlib as plt
 import plotly as py 
 import seaborn as sns
 import time 
+
+global_temp_country = pd.read_csv('/home/richard/Documents/Projects/globalearthtemp/GlobalLandTemperaturesByCountry.csv')
+print(global_temp_country)
